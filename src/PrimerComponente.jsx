11abbe7e7387c2c6db67482af7290506b263a6d7
Matias Daniel Alessandrello <matialess@gmail.com>
2025-05-16 21:43:58 -0300

    tipos de variables y como no podemos pasar objetos a los react childs

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,15 @@
-function PrimerComponente () {
-    return ( <h1>Hola Mundo!</h1>  );
-}
+const string = 'Esto es un texto'
+const number = 123456
+const array = ['Curso de React', 'Musica', 4, 1000]
+const boolean = true
+const funcion = () => 1+1
+const fecha = new Date()
+const objeto = {nombre: 'Curso de Javascript', duracion: 4}
 
-export default PrimerComponente ;
+
+
+export const PrimerComponente = () => {
+    return (
+        <p>{JSON.stringify(fecha)}</p>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import PrimerComponente from './PrimerComponente'
+import {PrimerComponente} from './PrimerComponente'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PrimerComponente/>
+    <PrimerComponente />
   </StrictMode>,
 )
