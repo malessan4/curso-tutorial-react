9d9e9686406b27490a89ed69ea15e6ce394e6654
Matias Daniel Alessandrello <matialess@gmail.com>
2025-05-16 22:05:51 -0300

    no se usan mas divs adentro de otros divs

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -2,14 +2,21 @@ const string = 'Esto es un texto'
 const number = 123456
 const array = ['Curso de React', 'Musica', 4, 1000]
 const boolean = true
-const funcion = () => 1+1
+const funcion = () => 1 + 1
 const fecha = new Date()
-const objeto = {nombre: 'Curso de Javascript', duracion: 4}
+const objeto = { nombre: 'Curso de Javascript', duracion: 4 }
 
 
 
 export const PrimerComponente = () => {
     return (
-        <p>{JSON.stringify(fecha)}</p>
+        <div>
+            <h1>Variable en JSX</h1>
+            <h4>Variable de tipo String:</h4> <p>{string}</p>
+            <h4>Variable de tipo Number:</h4> <p>{number}</p>
+            <h4>Variable de tipo array:</h4> <p>{array}</p>
+            <h4>Variable de tipo boolean:</h4> <p>{boolean}</p>
+            <h4>Variable de tipo funcion:</h4> <p>{funcion}</p>
+        </div>
     )
 }
